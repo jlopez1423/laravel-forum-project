78de029ca13e4bc858b0a6dc0b1ed74cf0f5ca04
Jose Lopez <jlopez@three29.com>
2017-10-11 16:57:20 -0700

    some changes

--- forum/app/Http/Controllers/ThreadController.php
+++ forum/app/Http/Controllers/ThreadController.php
@@ -14,7 +14,10 @@ class ThreadController extends Controller
      */
     public function index()
     {
-        //
+        
+        $threads = Thread::latest()->get();
+
+        return view( 'threads.index', compact( 'threads' ) );
     }
 
     /**
